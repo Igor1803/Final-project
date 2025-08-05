f9e03368f74746585a56131dc8f938f480f6f1db
Igor1803 <i.vinogtadov@gmail.com>
2025-08-05 19:53:56 +0300

    preparing for publication

--- config.py
+++
@@ -1,7 +0,0 @@
-# Конфигурация бота
-BOT_TOKEN = '7509898676:AAHQx-9g14m-ztU-qSeI0AWhJqonbkabUDI'
-OPERATOR_ID = 1986827259  # Замените на реальный Telegram ID оператора
-AUDIO_PATH = 'audio/'
-FAQ_PATH = 'data/faq.json'
-DIALOGS_PATH = 'data/dialogs.json'
-LOG_PATH = 'data/bot.log' 
